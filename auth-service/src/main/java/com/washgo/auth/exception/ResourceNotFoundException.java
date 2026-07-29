9cabd77c963b09556b635a237d12e2d6e765eac0
prakash45 <prakashpatil0235@gmail.com>
2026-07-29 19:11:58 +0530

    fix: update exception classes to extend RuntimeException for clean compilation

--- auth-service/src/main/java/com/washgo/auth/exception/ResourceNotFoundException.java
+++ auth-service/src/main/java/com/washgo/auth/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.washgo.auth.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
--- auth-service/src/main/java/com/washgo/auth/exception/UnauthorizedException.java
+++ auth-service/src/main/java/com/washgo/auth/exception/UnauthorizedException.java
@@ -1,4 +1,7 @@
 package com.washgo.auth.exception;
 
-public class UnauthorizedException {
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
 }
